f03742e39b799469b62078a679252108f689cc8e
Thomas Wiecki <thomas.wiecki@gmail.com>
2011-02-28 00:38:27 +0000

    Fixed setup.py importing numpy, thanks to Imri Sofer for reporting.

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-from distutils.core import setup
-from distutils.extension import Extension
-import numpy as np
-
 setup(
     name="kabuki",
     version="0.1",
@@ -9,8 +5,8 @@ setup(
     author_email="thomas_wiecki@brown.edu",
     url="http://code.google.com/p/kabuki",
     packages=["kabuki"],
-    description="HDDM is a python module that implements Hierarchical Bayesian estimation of Drift Diffusion Models.",
-    install_requires=['NumPy >=1.3.0', 'PyMC >=2.0'],
+    description="kabuki is a python toolbox that allows easy creation of hierarchical bayesian models for the cognitive sciences.",
+    install_requires=['NumPy >=1.3.0', 'PyMC >=2.0', 'ordereddict >= 1.1'],
     setup_requires=['NumPy >=1.3.0', 'PyMC >=2.0']
     )
 
