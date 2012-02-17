b535f5d66f4b0fa6bf8e1502d3d853750a17c83f
Imri Sofer <imrisofer@gmail.com>
2012-02-17 09:47:15 -0500

    checked out generate.py from master

---
+++ kabuki/generate.py
@@ -0,0 +1,88 @@
+from __future__ import division
+
+import numpy as np
+from copy import copy
+
+def _add_noise(params, noise=.1, exclude_params=()):
+    """Add individual noise to each parameter.
+
+        :Arguments:
+            params : dict
+                Parameter names and values
+
+        :Optional:
+            noise : float <default=.1>
+                Standard deviation of random gaussian
+                variable to add to each parameter.
+            exclude_params : tuple <default=()>
+                Do not add noise to these parameters.
+
+        :Returns:
+            params : dict
+                parameters with noise added.
+
+    """
+
+    params = copy(params)
+
+    for param, value in params.iteritems():
+        if param not in exclude_params:
+            params[param] = np.random.normal(loc=value, scale=noise)
+
+    return params
+
+def gen_rand_data(dist, params, samples=50, subjs=1, subj_noise=.1, exclude_params=()):
+    """Generate a random dataset using a user-defined random distribution.
+
+    :Arguments:
+        dist : kabuki.utils.scipy_stochastic
+            Probability distribution to sample from (has to have
+            random function defined).
+        params : dict
+            Parameters to use for data generation. Two options possible:
+                * {'param1': value, 'param2': value2}
+                * {'condition1': {'param1': value, 'param2': value2},
+                   'condition2': {'param1': value3, 'param2': value4}}
+            In the second case, the dataset is generated with multiple conditions
+            named after the key and will be sampled using the corresponding parameters.
+
+    :Optional:
+        samples : int <default: 50>
+            How many values to sample for each condition for each subject.
+        subjs : int <default: 1>
+            How many subjects to generate data from. Individual subject parameters
+            will be normal distributed around the provided parameters with variance
+            subj_noise if subjs > 1. If only one subject is simulated no noise is added.
+        subj_noise : float <default: .1>
+            How much to perturb individual subj parameters.
+        exclude_params : tuple <default ()>
+            Do not add noise to these parameters.
+
+    :Returns:
+        data : numpy structured array
+            Will contain the columns 'subj_idx', 'condition' and 'data' which contains
+            the random samples.
+
+    """
+    from itertools import product
+
+    # Check if only dict of params was passed, i.e. no conditions
+    if not isinstance(params[params.keys()[0]], dict):
+        params = {'none': params}
+
+    idx = list(product(range(subjs), params.keys(), np.float64(range(samples))))
+    data = np.array(idx, dtype=[('subj_idx', np.int32), ('condition', 'S20'), ('data', np.float64)])
+
+    for condition, param in params.iteritems():
+        for subj_idx in range(subjs):
+            if subjs > 1:
+                subj_param = _add_noise(param, noise=subj_noise, exclude_params=exclude_params)
+            else:
+                subj_param = param
+            samples_from_dist = dist.rv.random(size=samples, **subj_param)
+            idx = (data['subj_idx'] == subj_idx) & (data['condition'] == condition)
+            data['data'][idx] = np.array(samples_from_dist, dtype=np.float64)
+
+    return data
+
+
---
+++ kabuki/tests/test_generate.py
@@ -0,0 +1,106 @@
+import kabuki
+import numpy as np
+import unittest
+import scipy.stats
+from collections import OrderedDict
+from kabuki.generate import gen_rand_data, _add_noise
+
+normal_like = kabuki.utils.scipy_stochastic(scipy.stats.distributions.norm_gen, name='normal', longname='normal')
+
+class TestGenerate(unittest.TestCase):
+    def runTest(self):
+        pass
+
+    def test_add_noise(self):
+        noise = 1
+        params = OrderedDict([('loc', 0), ('scale', 1)])
+        np.random.seed(31337)
+        new_params = _add_noise(params, noise=noise)
+
+        # check if copied
+        self.assertFalse(new_params is params)
+
+        # check if noise correctly added
+        np.random.seed(31337)
+        self.assertTrue(new_params['loc'] == np.random.normal(loc=params['loc'], scale=noise))
+        self.assertTrue(new_params['scale'] == np.random.normal(loc=params['scale'], scale=noise))
+
+        # test whether exclude works
+        new_params = _add_noise(params, noise=noise, exclude_params=('scale',))
+        self.assertTrue(new_params['scale'] == 1)
+
+    def test_single_cond_no_subj(self):
+        params = {'loc': 0, 'scale': 1}
+        np.random.seed(31337)
+        data = gen_rand_data(normal_like, params, samples=100)
+        np.random.seed(31337)
+        truth = np.float64(normal_like.rv.random(size=100, **params))
+
+        np.testing.assert_array_equal(data['data'], truth)
+
+    def test_single_cond_multi_subjs(self):
+        params = OrderedDict([('loc', 0), ('scale', 1)])
+        subjs = 100
+        samples = 100
+
+        # generate test data
+        np.random.seed(31337)
+        data = gen_rand_data(normal_like, params, samples=samples, subjs=subjs)
+
+        # test subj present
+        np.testing.assert_array_equal(np.unique(data['subj_idx']), range(subjs))
+
+        # test for correct length
+        np.testing.assert_array_equal(len(data), subjs*samples)
+
+        # generate truth
+        np.random.seed(31337)
+        for i in range(subjs):
+            new_params = _add_noise(params)
+            truth = np.float64(normal_like.rv.random(size=samples, **new_params))
+            np.testing.assert_array_equal(data[data['subj_idx'] == i]['data'], truth)
+
+    def test_single_cond_multi_subjs_exclude(self):
+        params = OrderedDict([('loc', 0), ('scale', 1)])
+        subjs = 100
+        samples = 100
+
+        # generate test data
+        np.random.seed(31337)
+        data = gen_rand_data(normal_like, params, samples=samples, subjs=subjs, exclude_params=('scale',))
+
+        # test subj present
+        np.testing.assert_array_equal(np.unique(data['subj_idx']), range(subjs))
+
+        # test for correct length
+        np.testing.assert_array_equal(len(data), subjs*samples)
+
+        # generate truth
+        np.random.seed(31337)
+        for i in range(subjs):
+            new_params = _add_noise(params, exclude_params=('scale',))
+            truth = np.float64(normal_like.rv.random(size=samples, **new_params))
+            np.testing.assert_array_equal(data[data['subj_idx'] == i]['data'], truth)
+
+
+    def test_mulltiple_cond_no_subj(self):
+        samples = 100
+        params = OrderedDict([('cond1', {'loc': 0, 'scale': 1}), ('cond2', {'loc': 100, 'scale': 10})])
+
+        np.random.seed(31337)
+        data = gen_rand_data(normal_like, params, samples=samples)
+
+        # test whether conditions are present
+        np.testing.assert_array_equal(np.unique(data['condition']), ['cond1', 'cond2'])
+
+        # test for correct length
+        np.testing.assert_array_equal(len(data), 2*samples)
+
+        # generate truth
+        np.random.seed(31337)
+        truth = np.float64(normal_like.rv.random(size=100, **params['cond1']))
+        np.testing.assert_array_equal(data[data['condition'] == 'cond1']['data'], truth)
+
+        truth = np.float64(normal_like.rv.random(size=100, **params['cond2']))
+        np.testing.assert_array_equal(data[data['condition'] == 'cond2']['data'], truth)
+
