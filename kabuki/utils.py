a69e4dd3b5c03fa0bc5502a38ad3f307fe732753
Thomas Wiecki <thomas.wiecki@gmail.com>
2012-03-08 10:36:47 -0500

    ENH: Moved distribution stuff into new distributions.py. To remain compatibility with pymc 2.1 I copied the new_dist_class from current pymc 2.2.

--- kabuki/utils.py
+++ kabuki/utils.py
@@ -72,305 +72,6 @@ def load_csv(*args, **kwargs):
     #read data
     return np.recfromcsv(*args, **kwargs)
 
-def parse_config_file(fname, mcmc=False, load=False, param_names=None):
-    """Open, parse and execute a kabuki model as specified by the
-    configuration file.
-
-    :Arguments:
-        fname <str>: File name of config file.
-
-    :Optional:
-        mcmc <bool=False>: Run MCMC on model.
-        load <bool=False>: Load from database.
-    """
-    raise NotImplementedError("Obsolete, todo!")
-
-    import os.path
-    if not os.path.isfile(fname):
-        raise ValueError("%s could not be found."%fname)
-
-    import ConfigParser
-
-    config = ConfigParser.ConfigParser()
-    config.read(fname)
-
-    #####################################################
-    # Parse config file
-    data_fname = config.get('data', 'load')
-    if not os.path.exists(data_fname):
-        raise IOError, "Data file %s not found."%data_fname
-
-    try:
-        save = config.get('data', 'save')
-    except ConfigParser.NoOptionError:
-        save = False
-
-    data = np.recfromcsv(data_fname)
-
-    try:
-        model_type = config.get('model', 'type')
-    except ConfigParser.NoOptionError:
-        model_type = 'simple'
-
-    try:
-        is_subj_model = config.getboolean('model', 'is_subj_model')
-    except ConfigParser.NoOptionError:
-        is_subj_model = True
-
-    try:
-        no_bias = config.getboolean('model', 'no_bias')
-    except ConfigParser.NoOptionError:
-        no_bias = True
-
-    try:
-        debug = config.getboolean('model', 'debug')
-    except ConfigParser.NoOptionError:
-        debug = False
-
-    try:
-        dbname = config.get('mcmc', 'dbname')
-    except ConfigParser.NoOptionError:
-        dbname = None
-
-    # Get depends
-    depends = {}
-    if param_names is not None:
-        for param_name in param_names:
-            try:
-                # Multiple depends can be listed (separated by a comma)
-                depends[param_name] = config.get('depends', param_name).split(',')
-            except ConfigParser.NoOptionError:
-                pass
-
-    # MCMC values
-    try:
-        samples = config.getint('mcmc', 'samples')
-    except ConfigParser.NoOptionError:
-        samples = 10000
-    try:
-        burn = config.getint('mcmc', 'burn')
-    except ConfigParser.NoOptionError:
-        burn = 5000
-    try:
-        thin = config.getint('mcmc', 'thin')
-    except ConfigParser.NoOptionError:
-        thin = 3
-    try:
-        verbose = config.getint('mcmc', 'verbose')
-    except ConfigParser.NoOptionError:
-        verbose = 0
-
-    try:
-        plot_rt_fit = config.getboolean('stats', 'plot_rt_fit')
-    except ConfigParser.NoOptionError, ConfigParser.NoSectionError:
-        plot_rt_fit = False
-
-    try:
-        plot_posteriors = config.getboolean('stats', 'plot_posteriors')
-    except ConfigParser.NoOptionError, ConfigParser.NoSectionError:
-        plot_posteriors = False
-
-
-    print "Creating model..."
-
-    if mcmc:
-        if not load:
-            print "Sampling... (this can take some time)"
-            m.mcmc(samples=samples, burn=burn, thin=thin, verbose=verbose, dbname=dbname)
-        else:
-            m.mcmc_load_from_db(dbname=dbname)
-
-    if save:
-        m.save_stats(save)
-    else:
-        print m.summary()
-
-    if plot_rt_fit:
-        m.plot_rt_fit()
-
-    if plot_posteriors:
-        m.plot_posteriors
-
-    return m
-
-def scipy_stochastic(scipy_dist, **kwargs):
-    """
-    Return a Stochastic subclass made from a particular SciPy distribution.
-    """
-    import inspect
-    import scipy.stats.distributions as sc_dst
-    from pymc.ScipyDistributions import separate_shape_args
-    from pymc.distributions import new_dist_class, bind_size
-
-    if scipy_dist.__class__.__name__.find('_gen'):
-        scipy_dist = scipy_dist(**kwargs)
-
-    name = scipy_dist.__class__.__name__.replace('_gen','').capitalize()
-
-    (args, varargs, varkw, defaults) = inspect.getargspec(scipy_dist._pdf)
-
-    shape_args = args[2:]
-    if isinstance(scipy_dist, sc_dst.rv_continuous):
-        dtype=float
-
-        def logp(value, **kwds):
-            args, zkwds = separate_shape_args(kwds, shape_args)
-            if hasattr(scipy_dist, '_logp'):
-                return scipy_dist._logp(value, *args)
-            else:
-                return np.sum(scipy_dist.logpdf(value,*args,**kwds))
-
-        parent_names = shape_args + ['loc', 'scale']
-        defaults = [None] * (len(parent_names)-2) + [0., 1.]
-
-    elif isinstance(scipy_dist, sc_dst.rv_discrete):
-        dtype=int
-
-        def logp(value, **kwds):
-            args, kwds = separate_shape_args(kwds, shape_args)
-            if hasattr(scipy_dist, '_logp'):
-                return scipy_dist._logp(value, *args)
-            else:
-                return np.sum(scipy_dist.logpmf(value,*args,**kwds))
-
-        parent_names = shape_args + ['loc']
-        defaults = [None] * (len(parent_names)-1) + [0]
-    else:
-        return None
-
-    parents_default = dict(zip(parent_names, defaults))
-
-    def random(shape=None, **kwds):
-        args, kwds = separate_shape_args(kwds, shape_args)
-
-        if shape is None:
-            return scipy_dist.rvs(*args, **kwds)
-        else:
-            return np.reshape(scipy_dist.rvs(*args, **kwds), shape)
-
-    # Build docstring from distribution
-    docstr = name[0]+' = '+name + '(name, '+', '.join(parent_names)+', value=None, shape=None, trace=True, rseed=True, doc=None)\n\n'
-    docstr += 'Stochastic variable with '+name+' distribution.\nParents are: '+', '.join(parent_names) + '.\n\n'
-    docstr += """
-Methods:
-
-    random()
-        - draws random value
-          sets value to return value
-
-    ppf(q)
-        - percent point function (inverse of cdf --- percentiles)
-          sets value to return value
-
-    isf(q)
-        - inverse survival function (inverse of sf)
-          sets value to return value
-
-    stats(moments='mv')
-        - mean('m',axis=0), variance('v'), skew('s'), and/or kurtosis('k')
-
-
-Attributes:
-
-    logp
-        - sum(log(pdf())) or sum(log(pmf()))
-
-    cdf
-        - cumulative distribution function
-
-    sf
-        - survival function (1-cdf --- sometimes more accurate)
-
-    entropy
-        - (differential) entropy of the RV.
-
-
-NOTE: If you encounter difficulties with this object, please try the analogous
-computation using the rv objects in scipy.stats.distributions directly before
-reporting the bug.
-    """
-
-    new_class = new_dist_class(dtype, name, parent_names, parents_default, docstr, logp, random, True, None)
-    class newer_class(new_class):
-        __doc__ = docstr
-        rv = scipy_dist
-        rv.random = random
-
-        def __init__(self, *args, **kwds):
-            new_class.__init__(self, *args, **kwds)
-            self.args, self.kwds = separate_shape_args(self.parents, shape_args)
-            self.frozen_rv = self.rv(self.args, self.kwds)
-            self._random = bind_size(self._random, self.shape)
-
-        def _pymc_dists_to_value(self, args):
-            """Replace arguments that are a pymc.Node with their value."""
-            # This is needed because the scipy rv function transforms
-            # every input argument which causes new pymc lambda
-            # functions to be generated. Thus, when calling this many
-            # many times, excessive amounts of RAM are used.
-            new_args = []
-            for arg in args:
-                if isinstance(arg, pm.Node):
-                    new_args.append(arg.value)
-                else:
-                    new_args.append(arg)
-
-            return new_args
-
-        def pdf(self, value=None):
-            """
-            The probability distribution function of self conditional on parents
-            evaluated at self's current value
-            """
-            if value is None:
-                value = self.value
-            return self.rv.pdf(value, *self._pymc_dists_to_value(self.args), **self.kwds)
-
-        def cdf(self, value=None):
-            """
-            The cumulative distribution function of self conditional on parents
-            evaluated at self's current value
-            """
-            if value is None:
-                value = self.value
-            return self.rv.cdf(value, *self._pymc_dists_to_value(self.args), **self.kwds)
-
-        def sf(self, value=None):
-            """
-            The survival function of self conditional on parents
-            evaluated at self's current value
-            """
-            if value is None:
-                value = self.value
-            return self.rv.sf(self.value, *self._pymc_dists_to_value(self.args), **self.kwds)
-
-        def ppf(self, q):
-            """
-            The percentile point function (inverse cdf) of self conditional on parents.
-            Self's value will be set to the return value.
-            """
-            self.value = self.rv.ppf(q, *self._pymc_dists_to_value(self.args), **self.kwds)
-            return self.value
-
-        def isf(self, q):
-            """
-            The inverse survival function of self conditional on parents.
-            Self's value will be set to the return value.
-            """
-            self.value = self.rv.isf(q, *self._pymc_dists_to_value(self.args), **self.kwds)
-            return self.value
-
-        def stats(self, moments='mv'):
-            """The first few moments of self's distribution conditional on parents"""
-            return self.rv.stats(moments=moments, *self._pymc_dists_to_value(self.args), **self.kwds)
-
-        def _entropy(self):
-            """The entropy of self's distribution conditional on its parents"""
-            return self.rv.entropy(*self._pymc_dists_to_value(self.args), **self.kwds)
-        entropy = property(_entropy, doc=_entropy.__doc__)
-
-    newer_class.__name__ = new_class.__name__
-    return newer_class
 
 def set_proposal_sd(mc, tau=.1):
     for var in mc.variables:
@@ -391,6 +92,26 @@ def set_proposal_sd(mc, tau=.1):
 class _NoModuleFound(Exception):
     """
     No module was found because none exists.
+    """
+
+
+class InvalidName(ValueError):
+    """
+    The given name is not a dot-separated list of Python objects.
+    """
+
+
+class ModuleNotFound(InvalidName):
+    """
+    The module associated with the given name doesn't exist and it can't be
+    imported.
+    """
+
+
+class ObjectNotFound(InvalidName):
+    """
+    The object associated with the given name doesn't exist and it can't be
+    imported.
     """
 
 def _importAndCheckStack(importName):
