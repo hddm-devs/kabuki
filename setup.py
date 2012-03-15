4b02b27a0211fad32a119c0b8661743784f85d44
Imri Sofer <imrisofer@gmail.com>
2012-03-15 11:52:32 -0400

    Bumped version to 0.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="kabuki",
-    version="0.2RC4",
+    version="0.2",
     author="Thomas V. Wiecki, Imri Sofer",
     author_email="thomas.wiecki@gmail.com",
     url="http://github.com/hddm-devs/kabuki",
