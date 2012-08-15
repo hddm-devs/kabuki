4ec4b1b769193f5140746f9e13fb60bdf6145b72
Thomas Wiecki <thomas.wiecki@gmail.com>
2012-08-15 10:33:22 -0400

    Bumped version to 0.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name="kabuki",
-    version="0.3dev",
+    version="0.3",
     author="Thomas V. Wiecki, Imri Sofer",
     author_email="thomas.wiecki@gmail.com",
     url="http://github.com/hddm-devs/kabuki",
     packages=["kabuki"],
     description="kabuki is a python toolbox that allows easy creation of hierarchical bayesian models for the cognitive sciences.",
-    install_requires=['NumPy >=1.5.0', 'PyMC >=2.0', 'ordereddict >= 1.1', "pandas >= 0.8.0"],
-    setup_requires=['NumPy >=1.5.0', 'PyMC >=2.0', 'ordereddict >= 1.1', "pandas >= 0.8.0"]
+    install_requires=['NumPy >=1.5.0', 'PyMC >=2.2', 'ordereddict >= 1.1', "pandas >= 0.8.0"],
+    setup_requires=['NumPy >=1.5.0', 'PyMC >=2.2', 'ordereddict >= 1.1', "pandas >= 0.8.0"]
 )
