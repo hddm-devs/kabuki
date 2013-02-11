5f777b01f7fd1a1516972def594c4197ac361f2f
Thomas Wiecki <thomas.wiecki@gmail.com>
2013-02-11 17:17:59 -0500

    ENH: Added travis config.

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     url="http://github.com/hddm-devs/kabuki",
     packages=["kabuki"],
     description="kabuki is a python toolbox that allows easy creation of hierarchical bayesian models for the cognitive sciences.",
-    install_requires=['NumPy >= 1.5.0', 'PyMC >= 2.2', 'ordereddict >= 1.1', 'pandas >= 0.6.0', 'matplotlib >= 1.0.0'],
-    setup_requires=['NumPy >= 1.5.0', 'PyMC >= 2.2', 'ordereddict >= 1.1', 'pandas >= 0.6.0', 'matplotlib >= 1.0.0']
+    install_requires=['NumPy >= 1.5.0', 'PyMC >= 2.2', 'pandas >= 0.6.0', 'matplotlib >= 1.0.0'],
+    setup_requires=['NumPy >= 1.5.0', 'PyMC >= 2.2', 'pandas >= 0.6.0', 'matplotlib >= 1.0.0']
 )
