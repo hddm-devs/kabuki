195ce993caef381eed60661c7e83ece16f88e04e
Thomas Wiecki <thomas.wiecki@gmail.com>
2013-09-17 07:23:49 -0400

    DOC: Fixed version string.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="kabuki",
-    version="0.5.2.beta.dev",
+    version="0.5.2.beta",
     author="Thomas V. Wiecki, Imri Sofer",
     author_email="thomas.wiecki@gmail.com",
     url="http://github.com/hddm-devs/kabuki",
