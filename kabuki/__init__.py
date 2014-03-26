c1f369e789e6215d74c3c2089d885ef92787da77
twiecki <thomas.wiecki@gmail.com>
2014-03-26 10:50:54 +0900

    Removed dev from __init__.py.

--- kabuki/__init__.py
+++ kabuki/__init__.py
@@ -5,7 +5,7 @@ import analyze
 import step_methods as steps
 import generate
 
-__version__ = '0.5.4.dev'
+__version__ = '0.5.4'
 
 try:
     from IPython.core.debugger import Tracer; debug_here = Tracer()
