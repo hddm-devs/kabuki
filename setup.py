67a1985486d111a2e444a682ab0f93ddbcbeab9d
twiecki <thomas.wiecki@gmail.com>
2014-03-28 10:10:20 +0900

    BLD: Resolved conflict.

--- setup.py
+++ setup.py
@@ -8,11 +8,6 @@ setup(
     url="http://github.com/hddm-devs/kabuki",
     packages=["kabuki"],
     description="kabuki is a python toolbox that allows easy creation of hierarchical bayesian models for the cognitive sciences.",
-<<<<<<< HEAD
-    install_requires=['NumPy >= 1.6.0', 'pymc == 2.3.2', 'pandas >= 0.10.0', 'matplotlib >= 1.0.0'],
-    setup_requires=['NumPy >= 1.6.0', 'pymc == 2.3.2', 'pandas >= 0.10.0', 'matplotlib >= 1.0.0']
-=======
     install_requires=['NumPy >= 1.6.0', 'pymc == 2.3.2', 'pandas >= 0.12.0', 'matplotlib >= 1.0.0'],
     setup_requires=['NumPy >= 1.6.0', 'pymc == 2.3.2', 'pandas >= 0.12.0', 'matplotlib >= 1.0.0']
->>>>>>> e74214b... BLD: Updated pymc dependency.
 )
